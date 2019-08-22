1423ab7d1566d344a1be1f9169c790ae7cf2b01e
Kayla Petersen <petersen1994@gmail.com>
2019-08-22 14:54:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
   data.reduce({}) do |hash, (key, value)|
     value.each do |type, names|
       names.each do |name|
@@ -26,3 +27,20 @@ end
 #type = type of color 
 #name = name 
 #memo = {}
+=======
+  data.reduce({}) do |memo, (key, value)|
+    value.each do |type, names|
+      names.each do |name|
+        
+        if memo[name]
+          memo[name] << [type]
+        if memo[name][type]
+        
+          binding.pry
+          end
+        end
+      end
+    end
+  end
+end
+>>>>>>> 7217a74141323fbb41200a635add6ab88f8a6a2c
